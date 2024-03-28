045837d1184a791cb89aecdada5755451161e122
Minh-AnhHuynh <minh-anh.huynh@etu.u-paris.fr>
2024-03-28 15:50:15 +0100

    code: Exported the function to its own file

--- R/utils.R
+++ R/utils.R
@@ -102,62 +102,6 @@ rstudio_config_path <- function(...) {
 }
 
 
-#' List the online git repositories of the current user
-#'
-#' @param username string. The git username to list the repositories
-#'
-#' @return A list of the current public repositories for the current git user
-#' @export
-#' @examples
-#' list_github_repositories("cran")
-#' \dontrun{
-#' list_github_repositories()
-#' }
-#'
-list_github_repositories <- function(username = get_current_git_username) {
-  if (is.function(username)) {
-    username <- username()
-  }
-  # Set up the API endpoint0
-  url <- paste0("https://api.github.com/users/", username, "/repos")
-  # Make the HTTP request
-  response <- httr::GET(url)
-
-  # Check if the request was successful
-  if (httr::http_type(response) == "application/json") {
-    # Extract repository names
-    repos <- httr::content(response)
-    repo_names <- sapply(repos, function(repo) repo$svn_url)
-
-    # Print the list of repositories
-    cat("GitHub repositories for user", username, ":\n")
-    for (repo in repo_names) {
-      cat("- ", repo, "\n")
-    }
-  } else {
-    cat("Failed to retrieve repository information.\n")
-    cat("Response status:", httr::status_code(response), "\n")
-    cat("Response content:", httr::content(response, as = "text"), "\n")
-  }
-}
-
-
-
-#' Call the function to get the current Git username
-#'
-#' @return string: Current git username.
-#' @noRd
-#'
-#' @examples get_current_git_username()
-#'
-get_current_git_username <- function() {
-  # Run git config command to get the current user.name
-  config <- gert::git_config()
-  current_username <- config[config$name == "user.name", ]$value
-  # Print the current username
-  cat("Current Git username:", current_username, "\n")
-  return(current_username)
-}
 
 #' Get latest directory for import_from_github path
 #'
