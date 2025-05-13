800202398206b1adf972c3207d16e2d488270ce8
Minh-Anh Huynh <minh-anh.huynh@etu.u-paris.fr>
2025-05-13 19:20:21 +0200

    Correct function name usage

--- R/import_user_dictionary.R
+++ R/import_user_dictionary.R
@@ -2,14 +2,14 @@
 
 import_user_dictionary <- function(
     local_user_dict_path = "R/rstudio_preferences/",
-    user_dic_path = get_user_dictionary_path()
+    user_dic_path = get_user_dictionary_file()
 ) {
   # Copy the user dictionary
   user_dic_dir <- gsub( "user_dictionary", "", user_dic_path)
   user_dic_dir <- gsub("/", "\\\\", user_dic_dir) # Make it prettier to print in cli
   fs::dir_create(user_dic_dir)
-  user_file <- glue::glue({local_user_dict_path}, "user_dictionary/user_dictionary")
-  fs::file_copy(user_file, user_dic_path, overwrite = TRUE)
+  user_file <- glue::glue({local_user_dict_path}, "user_dictionary")
+  fs::file_copy(path = user_file, new_path = user_dic_path, overwrite = TRUE)
   cli::cli_alert_success(
     "User dictionary copied to {.path {user_dic_dir}}",
     wrap = TRUE
